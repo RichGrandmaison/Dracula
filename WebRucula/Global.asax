15c60849016fc585eec232f6fd6525107028b09c
MadReza <rezmaza@gmail.com>
2014-10-07 20:32:36 -0400

    Project WebRecula

---
+++ WebRucula/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebRucula.MvcApplication" Language="C#" %>
---
+++ WebRucula/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebRucula.Startup))]
+namespace WebRucula
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
